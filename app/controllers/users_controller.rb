0a83ebe7122bb3be227493f83ded3f743e920701
wisanu <wisanu@odds.team>
2024-09-04 20:19:33 +0700

    set route for new functions in controller and html table show data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-
   before_action :set_user, only: [:edit, :update, :destroy]
 
-  def index 
+  def index
     @users = User.all
   end
 
@@ -13,15 +12,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, notice: 'User was successfully created.'
+      redirect_to users_path, notice: 'User was successfully created.' # Redirect ไปหน้า index
     else
       render :new
     end
   end
 
+  def edit
+  end
+
   def update
     if @user.update(user_params)
-      redirect_to new_user_path, notice: 'Registration was successfully updated.'
+      redirect_to users_path, notice: 'User was successfully updated.' # Redirect ไปหน้า index
     else
       render :edit
     end
@@ -29,15 +31,16 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to new_user_path, notice: 'Registration was successfully deleted.'
+    redirect_to users_path, notice: 'User was successfully deleted.' # Redirect ไปหน้า index
   end
 
   private
-    def set_uesr
-      @user = Registration.find(params[:id])
-    end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone, :subject)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone, :subject)
+  end
 end
