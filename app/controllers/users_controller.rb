db53ff698d6c5cda3e7faeca5a8ea7b4f520e92a
wisanu <wisanu@odds.team>
2024-09-04 21:15:05 +0700

    revers to begin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :destroy]
-  def index
-    @users = User.all
-  end
   def new
     @user = User.new
   end
@@ -10,33 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path, notice: 'User was successfully created.' # Redirect ไปหน้า index
+      redirect_to new_user_path, notice: 'User was successfully created.'
     else
       render :new
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to users_path, notice: 'User was successfully updated.' # Redirect ไปหน้า index
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @user.destroy
-    redirect_to users_path, notice: 'User was successfully deleted.' # Redirect ไปหน้า index
-  end
-
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone, :subject)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+  get 'users/new'
+  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:new, :create]
 end
