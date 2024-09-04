ca8c9e580459e75b55c0fcc0da4137a40060c7e6
wisanu <wisanu@odds.team>
2024-09-04 21:16:12 +0700

    New route resources :users, only: [:new, :create, :index, :destroy]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'users/new'
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :index, :destroy]
 end
